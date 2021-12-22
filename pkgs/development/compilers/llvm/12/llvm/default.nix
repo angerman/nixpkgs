548cbebec880c675f885311050ca4157e7996efa
Moritz Angermann <moritz.angermann@gmail.com>
2021-12-22 17:56:46 +0800

    Update default.nix

--- pkgs/development/compilers/llvm/12/llvm/default.nix
+++ pkgs/development/compilers/llvm/12/llvm/default.nix
@@ -58,6 +58,9 @@ in stdenv.mkDerivation (rec {
     ++ [ zlib ];
 
   patches = [
+    # Fix llvm being miscompiled by some gccs. See llvm/llvm-project#49955
+    ./fix-llvm-issue-49955.patch
+    
     ./gnu-install-dirs.patch
     # On older CPUs (e.g. Hydra/wendy) we'd be getting an error in this test.
     (fetchpatch {
